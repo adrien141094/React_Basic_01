de32c107521e34599f5557b1aac5a8241a27c1c4
Adrien Turcek-Perron <adrien141094@gmail.com>
2023-03-28 10:17:26 +0200

    Creation premier composant

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,12 @@
+
+function PokemonCard() {
+  return <div>
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+      alt="bulbasaur_image" />
+    <figcaption>An Pokémon Bulbasaur</figcaption>
+
+  </div>
+}
+
+
+export default PokemonCard;
