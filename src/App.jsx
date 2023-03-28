7d52aa94d68832825261f2aa0be32697975f35d9
Adrien Turcek-Perron <adrien141094@gmail.com>
2023-03-28 22:33:59 +0200

    Exo 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 import "./App.css";
+
 function App() {
 
   const pokemonList = [
@@ -9,13 +11,41 @@ function App() {
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
       name: "mew",
     },
-  ]
+  ];
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handleClickNext = () => {
+    setPokemonIndex(pokemonIndex + 1)
+  };
+
+  const handleClickPrevious = () => {
+    setPokemonIndex(pokemonIndex - 1)
+  };
+
 
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      <button onClick={handleClickNext}>Suivant</button>
+      <button onClick={handleClickPrevious}>Précédent</button>
     </div>
   )
 }
