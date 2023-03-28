70bfa2bb26250241a3c3ccf50eebe47a03154c7a
Adrien Turcek-Perron <adrien141094@gmail.com>
2023-03-28 12:24:32 +0200

    Tableau de Pokémon

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,25 @@
 
 function PokemonCard() {
-  return <div>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-      alt="bulbasaur_image" />
-    <figcaption>An Pokémon Bulbasaur</figcaption>
+  const pokemon = pokemonList[1];
+  return <div className="card">
+    {pokemon.imgSrc ? (
+      <img src={pokemon.imgSrc}
+        alt={pokemon.name} className="card-img" />) : (<p>???</p>)}
+    <figcaption>{pokemon.name}</figcaption>
 
   </div>
 }
+
+const pokemonList = [
+  {
+    name: "Bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 
 export default PokemonCard;
